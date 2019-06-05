77119bfbca0dd3a620c70c957e9f41e0f1402993
Mickaël <mickael.caro@sonarsource.com>
2019-06-05 13:12:13 +0200

    Added codesmell

--- WithTypeScript/WithTypeScript/ClientApp/src/app/home/home.component.ts
+++ WithTypeScript/WithTypeScript/ClientApp/src/app/home/home.component.ts
@@ -5,4 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+
+  const password = "asdasd";
+
+  let my_pwd;
+my_pwd = "qwerewt";
+
+//login({ passwd: "zxvxcv" });
+
+const url = "https://example.com?password=hl2OAIXXZ60";
 }
